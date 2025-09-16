2b1f5a6b352c1feb984e7dbfbafca80e73df92c7
GNONLONFINfrankel <frankelgnonlonfin1@gmail.com>
2025-09-16 12:10:17 +0100

    true

--- app/config.py
+++ app/config.py
@@ -1,17 +1,24 @@
 import os
 
-# filepath: c:\Users\Frankel\hackathon\UniPlanBj\app\config.py
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'un_secret_unique_et_long_ici'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Cyrusgnl2007@localhost/UNIPLANBJ'
+
+    # Base de données distante fournie par Railway
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_PORT = os.environ.get('DB_PORT', 3306)  # MySQL par défaut
+
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     ADMIN_PIN = '200716'
 
-    # Configuration de Flask-Mail (exemple avec Gmail)
-    # IMPORTANT: Utilisez des variables d'environnement en production
+    # Configuration Flask-Mail
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # Votre adresse e-mail
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # Le mot de passe d'application de votre e-mail
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = MAIL_USERNAME
